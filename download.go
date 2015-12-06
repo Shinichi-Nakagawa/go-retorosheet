c49d00e96d88d252da0216e471e7601281a196bf
shinyorke <spirits.is.my.rader@gmail.com>
2015-12-06 22:43:33 +0900

    create dataset for event & game

--- download.go
+++ download.go
@@ -4,21 +4,32 @@
 package main
 
 import (
+	"archive/zip"
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"io/ioutil"
 	"path"
-	"flag"
+	"path/filepath"
 	"sync"
 )
 
+const (
+	// DirName a saved download file directory
+	DirName = "files"
+)
 
+// IsExist return a file exist
 func IsExist(filename string) bool {
-    _, err := os.Stat(filename)
-    return err == nil
+
+	// Exists check
+	_, err := os.Stat(filename)
+	return err == nil
 }
 
+// MakeWorkDirectory a make work directory
 func MakeWorkDirectory(dirname string) {
 	if IsExist(dirname) {
 		return
@@ -26,7 +37,9 @@ func MakeWorkDirectory(dirname string) {
 	os.MkdirAll(dirname, 0777)
 }
 
-func DownloadArchives(url string, dirname string) {
+// DownloadArchives a download files for retrosheet
+func DownloadArchives(url string, dirname string) string {
+
 	// get archives
 	fmt.Println(fmt.Sprintf("download: %s", url))
 	response, err := http.Get(url)
@@ -44,7 +57,8 @@ func DownloadArchives(url string, dirname string) {
 	}
 	_, filename := path.Split(url)
 	fmt.Println(filename)
-	file, err := os.OpenFile(fmt.Sprintf("%s/%s", dirname, filename), os.O_CREATE|os.O_WRONLY, 0777)
+	fullfilename := fmt.Sprintf("%s/%s", dirname, filename)
+	file, err := os.OpenFile(fullfilename, os.O_CREATE|os.O_WRONLY, 0777)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -53,14 +67,52 @@ func DownloadArchives(url string, dirname string) {
 	}()
 
 	file.Write(body)
+
+	return fullfilename
+
 }
 
+// Unzip return error a open read & write archives
+func Unzip(fullfilename string, outputdirectory string) error {
+	r, err := zip.OpenReader(fullfilename)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
 
-func GetEventsFileUrl(year int) string {
+		path := filepath.Join(outputdirectory, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			f, err := os.OpenFile(
+				path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// GetEventsFileURL return a events file URL
+func GetEventsFileURL(year int) string {
 	return fmt.Sprintf("http://www.retrosheet.org/events/%deve.zip", year)
 }
 
-func GetGameLogsUrl(year int) string {
+// GetGameLogsURL return a game logs URL
+func GetGameLogsURL(year int) string {
 	return fmt.Sprintf("http://www.retrosheet.org/gamelogs/gl%d.zip", year)
 }
 
@@ -70,24 +122,27 @@ func main() {
 	var toYear = flag.Int("t", 2014, "Season Year(To)")
 	flag.Parse()
 
-	// make dir
-	var dirname string = "files"
-	MakeWorkDirectory(dirname)
+	MakeWorkDirectory(DirName)
 
 	wait := new(sync.WaitGroup)
 	// Generate URL
 	urls := []string{}
-	for year := *fromYear; year < *toYear + 1; year++ {
-		urls = append(urls, GetEventsFileUrl(year))
+	for year := *fromYear; year < *toYear+1; year++ {
+		urls = append(urls, GetEventsFileURL(year))
 		wait.Add(1)
-		urls = append(urls, GetGameLogsUrl(year))
+		urls = append(urls, GetGameLogsURL(year))
 		wait.Add(1)
 	}
 
 	// Download files
 	for _, url := range urls {
 		go func(url string) {
-			DownloadArchives(url, dirname)
+			fullfilename := DownloadArchives(url, DirName)
+			err := Unzip(fullfilename, DirName)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 			wait.Done()
 		}(url)
 	}
